1df98f7b917792d9b25319dc41e0e09bd130db8e
sandi018u <sandi.gonzalez.ico23@gmail.com>
2019-09-05 10:07:08 -0500

    Es un archivo para pedirte el nombre la carrera y edad de un estudiante atraves de estructuras

--- ejemplo2.cpp
+++ ejemplo2.cpp
@@ -1,20 +1,48 @@
-#include <bits/stdc++.h>
-using namespace std;
-int numeros[10];
+#include <iostream>
+#include<conio.h>
+using namespace std; 
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct direccion{
+	char calle[20];
+	int cp;
+	char ciudad[20];
+	
+}; 
+struct persona{
+	char nombre [20];
+	int edad;
+	float estatura;
+	struct direccion dire;
+};
 
 
-int main(int argc, char** argv) {
-
-	int mayor =INT_MIN;
-	int *puntero =numeros;
-	for (;puntero<=&numeros[9];puntero++){
-		cout<<"Dame un numero ";
-		cin>>*puntero;
-		if(*puntero>mayor) mayor=*puntero;
+int main(int argc, char** argv){
+	struct persona P1[5];
+	struct persona *apuntador;
+	apuntador=&P1[0];
+	
+	for (int i=0;i<1;i++){
+		cout<<"nombre: ";
+		cin.getline(P1[i].nombre,20,'\n');
+		cout<<"Edad: ";
+		cin>>P1[i].edad;
+		cout<<"Estatura: ";
+		cin>>P1[i].estatura;
+		cout<<"Calle: ";
+		fflush(stdin);
+		cin.getline(P1[i].dire.calle,20,'\n');
+		
 	}
-	cout<<"El numero mas grande es: "<<mayor<<endl;
-	cout<<"direccion de memoria del arreglo"<<&numeros[10]<<endl;
-	cout<<"direccion del puntero"<<puntero<<endl;
-
+	//mostrar o gardado 
+	cout<<endl<<"nombre guardado: "<<apuntador->nombre;
+	cout<<endl<<"edad guardada: "<<P1[0].edad;
+	cout<<endl<<"estatura guardada"<<P1[0].estatura;
+	cout<<endl<<"calle guardada: "<<P1[0].dire.calle;
+	
+	
+	
+	getch();
 	return 0;
 }
