b0f21a09e397732e3b423e53aea113a15099e631
sandi018u <sandi.gonzalez.ico23@gmail.com>
2019-09-12 21:27:11 -0500

    Es un programa para dar de alta a un usuario a la subscripcion de una revista en el cual tambien se puede dar de baja en cualquier moomento por medio de punteros

--- ejemplo2.cpp
+++ ejemplo2.cpp
@@ -1,48 +1,79 @@
 #include <iostream>
 #include<conio.h>
-using namespace std; 
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
-struct direccion{
-	char calle[20];
-	int cp;
-	char ciudad[20];
-	
-}; 
-struct persona{
-	char nombre [20];
-	int edad;
-	float estatura;
-	struct direccion dire;
+struct suscripcion{
+    char nombre[20];
+    int tiempo;
+    char nombreR[20];
 };
 
+int main(){
+    struct suscripcion P[10];
+    struct suscripcion *apuntador;
+    apuntador=P;
+    int a;
+    char eliminar[20];
+    int opcion;
+    for(int i=0; i<10; i++){
+        P[i].nombre[0]='n';
+    cout<<"MENU"<<endl;
+	cout<<"1. Dar de alta un dato"<<endl;
+	cout<<"2. Eliminar dato"<<endl;
+	cout<<"3. Mostrar datos"<<endl;
+	cout<<"4. Salir"<<endl;
+	cout<<"Que deseas hacer: ";
+    cin>>opcion;
+    switch (opcion){
+        case 1:
+            for(int i=0;i<10; i++){
+                if(P[i].nombre[0]='n'){
+                    cout<<"Nombre: "; fflush(stdin);cin.getline(P[i].nombre,20,'\n');
+                    cout<<"Tiempo del que desea la Suscripcion: "; cin>>P[i].tiempo;
+                    cout<<"Nombre de la revista a la que esta suscrito: "; fflush(stdin); cin.getline(P[i].nombreR,20,'\n');
+                    cout<<'\n';
+                    break;
+                    }else
+                    cout<<"El "<<i+1<<" espacio esta ocupado"<<endl;
+        }break;
+        case 2:
+            cout<<"Nombre del usuario que desea dar de baja: ";
+			fflush(stdin);
+			cin.getline(eliminar,20,'\n');
+            for(int i=0; i<10; i++){
+a=strcmp(apuntador->nombre,eliminar);
+            	if (a!=0)
+            		apuntador++;
+            	else{
+					P[i].nombre[0]='n';
+                    cout<<"Usuario eliminado"<<endl;
+				}
+			}
 
-int main(int argc, char** argv){
-	struct persona P1[5];
-	struct persona *apuntador;
-	apuntador=&P1[0];
+            break;
+
+        case 3:
+        	cout<<"Datos de la estructura";
+        	for(int i=0; i<'n'; i++){
+        			cout<<"DATOS GUARDADOS"<<endl;
+       				cout<<"Nombre: "<<apuntador->nombre<<endl;
+        			cout<<"Tiempo de suscripcion: "<<apuntador->tiempo<<endl;
+        			cout<<"Nombre de la revista: "<<apuntador->nombreR<<endl;
+        		}
+				break;
+        default:
+            cout<<"Fin del proceso"<<endl;
+            break;
+
+    }
+
+    
+ } while (opcion != 4); 
 	
-	for (int i=0;i<1;i++){
-		cout<<"nombre: ";
-		cin.getline(P1[i].nombre,20,'\n');
-		cout<<"Edad: ";
-		cin>>P1[i].edad;
-		cout<<"Estatura: ";
-		cin>>P1[i].estatura;
-		cout<<"Calle: ";
-		fflush(stdin);
-		cin.getline(P1[i].dire.calle,20,'\n');
-		
-	}
-	//mostrar o gardado 
-	cout<<endl<<"nombre guardado: "<<apuntador->nombre;
-	cout<<endl<<"edad guardada: "<<P1[0].edad;
-	cout<<endl<<"estatura guardada"<<P1[0].estatura;
-	cout<<endl<<"calle guardada: "<<P1[0].dire.calle;
-	
-	
-	
-	getch();
+
 	return 0;
 }
